d4bd81f73c4384e7a8b4e11aa6fb9577ff77633a
dinghaixiang <1004836567@qq.com>
2017-12-27 15:43:37 +0800

    feat(period)scheduled 定时任务

---
+++ src/main/java/com/example/demo/service/ScheduleTaskService.java
@@ -0,0 +1,30 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.AuthContext;
+import com.example.demo.utils.MapUtils;
+import com.example.demo.utils.eql.Dql;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * Created by beck on 2017/12/27.
+ */
+@Component
+public class ScheduleTaskService {
+    @Autowired
+    private LeadService service;
+
+    @Scheduled(cron = "0 0 0 * * ?")//每天凌晨3:00执行任务
+    public void auto() {
+        System.out.print("ScheduleTask=============start");
+        AuthContext.setUserId("1");
+        Map map=service.income();
+        map.put("netInvestmentTotal", MapUtils.getInt(map,"currentMonthTotalInvest")-MapUtils.getInt(map,"currentMonthTotalInvestComed"));
+        map.put("netInvestment", MapUtils.getInt(map,"currentMonthInvest")-MapUtils.getInt(map,"currentMonthInvestComed"));
+        new Dql().insert("insertStatics").params(map).execute();
+        System.out.print("ScheduleTask=============end");
+    }
+}
